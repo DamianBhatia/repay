0dd42c3a9f119009002298a9e4e41ac3cd76d810
DamianBhatia <damian.bhatia@gmail.com>
2021-10-16 20:37:44 -0400

    dev - init Login and SignUp page

--- repay-app/App.js
+++ repay-app/App.js
@@ -1,8 +1,11 @@
-import React from 'react';
-import { Text } from 'react-native';
+import React from 'react'
+import { Text } from 'react-native'
+
+import Login from './Login/Login'
+import SignUp from './SignUp/SignUp'
 
 export default App = () => {
   return (
-    <Text>Hello World</Text>
+    <SignUp />
   )
 }
---
+++ repay-app/SignUp/SignUp.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Text } from 'react-native'
+
+const SignUp = () => {
+    return (
+        <Text>Signup Page</Text>
+    )
+}
+
+export default SignUp
